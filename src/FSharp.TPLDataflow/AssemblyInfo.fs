06a0a1a2457d9509ed927fa481e7f2944a856806
Jimmy Byrd <jimmybyrd@gmail.com>
2015-07-07 18:49:13 -0400

    Project Scaffold

---
+++ src/FSharp.TPLDataflow/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSharp.TPLDataflow")>]
+[<assembly: AssemblyProductAttribute("FSharp.TPLDataflow")>]
+[<assembly: AssemblyDescriptionAttribute("An FSharp wrapper libray for TPL Dataflow")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
