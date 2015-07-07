b89d36b9de5038dcc7cb99db6142ec6ad3363927
Jimmy Byrd <jimmybyrd@gmail.com>
2015-07-07 19:09:49 -0400

    Initial wrapper design

--- src/FSharp.TPLDataflow/Library.fs
+++ src/FSharp.TPLDataflow/Library.fs
@@ -1,16 +1,44 @@
-﻿namespace FSharp.TPLDataflow
+﻿
+module FSharp.TPLDataflow 
 
-/// Documentation for my library
-///
-/// ## Example
-///
-///     let h = Library.hello 1
-///     printfn "%d" h
-///
-module Library = 
-  
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+open System
+open System.Threading.Tasks
+open System.Threading.Tasks.Dataflow
+
+let inline action f = new Action<_>(f)
+let inline predicate f = new Predicate<_>(f)
+let private defaultExecutionOptions = new ExecutionDataflowBlockOptions()
+let inline startAsPlainTask (work : Async<unit>) = Task.Factory.StartNew(fun () -> work |> Async.RunSynchronously)
+
+//execution blocks
+let actionBlockWithOptions f opt = ActionBlock<_>(f |> action, opt)
+let actionBlock f = actionBlockWithOptions f defaultExecutionOptions
+let actionBlockAsyncWithOptions (f: 'a -> Async<unit>) opt = ActionBlock<_>(f >> startAsPlainTask, opt)
+let actionBlockAsync f = actionBlockAsyncWithOptions f defaultExecutionOptions
+   
+let transformBlockWithOptions (f: 'a -> 'b) opt = TransformBlock<_,_>(f, opt)
+let transformBlock f = transformBlockWithOptions f defaultExecutionOptions
+let transformBlockAsyncWithOptions (f : 'a -> Async<'b>) opt = TransformBlock<'a,'b>(f >> Async.StartAsTask, opt)
+let transformBlockAsync f = transformBlockAsyncWithOptions f defaultExecutionOptions
+
+let transformManyBlockWithOptions (f : 'a -> seq<'b>) opt = TransformManyBlock<_,_>(f, opt)
+let transformManyBlock (f : 'a -> seq<'b>) = transformManyBlockWithOptions f defaultExecutionOptions    
+let transformManyBlockAsyncWithOptions (f : 'a -> Async<seq<'b>>) opt =  TransformManyBlock(f >> Async.StartAsTask, opt)
+let transformManyBlockAsync (f : 'a -> Async<seq<'b>>) = transformManyBlockAsyncWithOptions f defaultExecutionOptions
+
+let linkToWithOptionsAndFilter target opt pred (source : ISourceBlock<_>) =
+    source.LinkTo(target, opt, pred |> predicate)
+let linkToWithOptions target opt (source : ISourceBlock<_>) =
+    source.LinkTo(target, opt)
+let linkToWithFilter target pred (source : ISourceBlock<_>) =
+    source.LinkTo(target, pred |> predicate)
+let linkTo target (source : ISourceBlock<_>) =
+    source.LinkTo(target)
+
+let (&>) source target = linkTo target source
+
+let toObservable (source : ISourceBlock<_>) = source.AsObservable()
+let toObserver (target : ITargetBlock<_>) = target.AsObserver()
+
+let (!>) message (target : ITargetBlock<_>)  = target.SendAsync(message)
+let (<!) (target : ITargetBlock<_>) message  = target.SendAsync(message)  
--- tests/FSharp.TPLDataflow.Tests/Tests.fs
+++ tests/FSharp.TPLDataflow.Tests/Tests.fs
@@ -3,8 +3,4 @@ module FSharp.TPLDataflow.Tests
 open FSharp.TPLDataflow
 open NUnit.Framework
 
-[<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+
